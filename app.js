7f81f371c01cb258b7105c2397f723a777f92492
Abbie <77949813+Abbie-Bowen@users.noreply.github.com>
2021-08-03 15:53:12 -0700

    setting up express server

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var path = require('path');
+// var data = require('/data.json');
+
+var app = express();
+
+// // view engine 
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'pug');
+
+//set static folder
+// app.use(express.static(path.join(__dirname, 'public')));
+
+
+//listening
+const PORT = process.envPORT || 5000;
+app. listen(PORT, () => console.log(`server started on port ${PORT}`));
