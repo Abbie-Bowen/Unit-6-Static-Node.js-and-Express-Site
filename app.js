c5fb511493e45dc58e975e01d12a9c9910118cc0
Abbie <77949813+Abbie-Bowen@users.noreply.github.com>
2021-08-04 19:47:28 -0700

    Templates rendering, working on connecting data file to templates

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var path = require('path');
-var projects = require('./data.json');
+var {projects} = require('./data.json');
 
 var app = express();
 
@@ -13,18 +13,18 @@ app.use('/static', express.static('public'));
 
 //routes
 app.get('/', (req, res, next) => {
-    res.render('index');
+    res.render('index', {projects});
 });
 
 app.get('/about', (req, res, next) => {
     res.render('about');
 });
 
-app.get('/project', (req, res, next) => {
+app.get('/project/:id', (req, res, next) => {
     res.render('project');
 });
 
 
 //listening
-const PORT = process.envPORT || 5000;
+const PORT = process.envPORT || 3000;
 app. listen(PORT, () => console.log(`server started on port ${PORT}`));
